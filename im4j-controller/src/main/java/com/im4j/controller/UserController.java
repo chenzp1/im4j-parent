88039830698f064f30e4d23aacda30beb44c8800
chenzp <847979339@qq.com>
2017-09-14 16:51:40 +0800

    加入shiro

--- im4j-controller/src/main/java/com/im4j/controller/UserController.java
+++ im4j-controller/src/main/java/com/im4j/controller/UserController.java
@@ -4,11 +4,18 @@ import com.im4j.cmmon.Result;
 import com.im4j.exception.ClientException;
 import com.im4j.pojo.User;
 import com.im4j.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.apache.shiro.web.filter.mgt.DefaultFilter.user;
 
 
 /**
@@ -31,6 +38,25 @@ public class UserController {
     @ResponseBody
     public Result register(@RequestBody  User user){
         return  Result.getInstance(userService.register(user));
+    }
+
+    /**
+     * 用户登陆
+     * @param request
+     * @return
+     */
+    @RequestMapping("login")
+    @ResponseBody
+    public Result login(HttpServletRequest request){
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken(request.getParameter("username"), request.getParameter("password"));
+            subject.login(token);//会跳到我们自定义的realm中
+            request.getSession().setAttribute("user", user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.getInstance("");
     }
 
 
